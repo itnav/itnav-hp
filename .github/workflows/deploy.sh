dee039d872b7bd63dc1cfd56f12ce32ffd148794
junpei-8 <junpei.v8@gmail.com>
2024-05-17 13:37:05 +0900

    update: デプロイテストの修正

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,20 +5,14 @@ mkdir -p ~/.ssh
 echo "$VPS_SSH_KEY" > ~/.ssh/vps
 chmod 600 ~/.ssh/vps
 
-# # SSH 接続設定
+# SSH 接続設定
 ssh-keyscan $VPS_HOST >> ~/.ssh/known_hosts
 
-echo "Host: $VPS_HOST"
-echo "Port: $VPS_PORT"
-echo "User: $VPS_USER"
-cat ~/.ssh/vps
-
-# # SSH 接続設定の重複削除
-# sort -u ~/.ssh/known_hosts -o ~/.ssh/known_hosts
+# SSH 接続設定の重複削除
+sort -u ~/.ssh/known_hosts -o ~/.ssh/known_hosts
 
 # 環境変数のリストを生成
 env_list=$(compgen -v | awk '{printf "$%s,", $0}' | sed 's/,$//')
 
 # EC2 へ SSH 接続して Shell を実行
-# ssh -i ~/.ssh/vps -p $VPS_PORT $VPS_USER@$VPS_HOST "echo $(envsubst $env_list < ./.github/workflows/deploy.ssh.sh) | bash"
-ssh -i ~/.ssh/vps -p $VPS_PORT $VPS_USER@$VPS_HOST "echo $(envsubst $env_list < ./.github/workflows/deploy.ssh.sh) | bash"
+ssh -i ~/.ssh/vps -p $VPS_PORT $VPS_USER@$VPS_HOST
