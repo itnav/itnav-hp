d64fcba2549ddd4d304787c4820ba7c0345f3a8b
junpei-8 <junpei.v8@gmail.com>
2024-05-17 16:47:09 +0900

    update: リモート SSH で実行した際にエラーが発生したら、こちらでもエラーを起こすように仮実装

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,5 @@
 # SSH key を用意
-echo "\n[Preparing SSH key.]"
+echo -e "\n[Preparing SSH key.]"
 mkdir -p ~/.ssh
 echo "$VPS_SSH_KEY" > ~/.ssh/vps
 chmod 600 ~/.ssh/vps
@@ -8,5 +8,10 @@ chmod 600 ~/.ssh/vps
 env_list=$(compgen -v | awk '{printf "$%s,", $0}' | sed 's/,$//')
 
 # EC2 へ SSH 接続して Shell を実行
-echo "\n[Connecting to VPS and executing shell.]"
+echo -e "\n[Connecting to VPS and executing shell.]"
 ssh -i ~/.ssh/vps -p $VPS_PORT -o StrictHostKeyChecking=no $VPS_USER@$VPS_HOST "echo $(envsubst $env_list < ./.github/workflows/deploy.ssh.sh) | bash"
+
+if [ $? -ne 0 ]; then
+	echo -e "\nRemote command failed"
+  exit 1
+fi
--- .github/workflows/deploy.ssh.sh
+++ .github/workflows/deploy.ssh.sh
@@ -1,11 +1,13 @@
 # 前提: 実行元のサーバーは GitHub に対する SSH 公開鍵を登録している前提です。
 
+exit 1;
+
 # Application ディレクトリを作成
 mkdir -p $APP_DIR
 
 # Git の設定
 if [ -d "$APP_DIR/.git" ]; then
-	echo "\n[Updating the "$APP_DIR" repository.]"
+	echo -e"\n[Updating the "$APP_DIR" repository.]"
 
 	# 作業先へ移動
 	cd $APP_DIR
@@ -16,7 +18,7 @@ if [ -d "$APP_DIR/.git" ]; then
 	sudo git reset --hard origin/$GITHUB_BRANCH_NAME
 
 else
-	echo "\n[Cloning the "$GITHUB_REPOSITORY_NAME" repository.]"
+	echo -e "\n[Cloning the "$GITHUB_REPOSITORY_NAME" repository.]"
 
 	# Git リポジトリをクローン
 	git clone git@github.com:$GITHUB_REPOSITORY_NAME.git $APP_DIR
@@ -28,15 +30,15 @@ else
 fi
 
 # 環境変数ファイルの生成
-echo "\n[Generating the .env file.]"
+echo -e "\n[Generating the .env file.]"
 echo "$APP_ENV" > .env
 
 # Docker Compose の実行
-echo "\n[Stopping the Docker Compose.]"
+echo -e "\n[Stopping the Docker Compose.]"
 docker compose down
 
-echo "\n[Building and running the Docker Compose.]"
+echo -e "\n[Building and running the Docker Compose.]"
 docker compose build
 
-echo "\n[Starting the Docker Compose.]"
+echo -e "\n[Starting the Docker Compose.]"
 docker compose up -d
