7854d2a9b95aa82c32022a9f39f384aaabc6bcf0
junpei-8 <junpei.v8@gmail.com>
2024-05-17 15:38:54 +0900

    update: ビルドエラーが発生した箇所を修正

--- src/pages/blogs.json.ts
+++ src/pages/blogs.json.ts
@@ -2,6 +2,8 @@ import type { APIContext, APIRoute } from 'astro';
 import { fetchBlogs } from '@/api/cms/fetch/blogs';
 import { defineJsonEndpoint } from '@/api/response';
 
+export const prerender = false;
+
 export type GetResponse = Awaited<ReturnType<typeof get>>;
 export const GET: APIRoute = (context) => defineJsonEndpoint(get, context);
 async function get(_: APIContext) {
--- src/pages/blogs/categories.json.ts
+++ src/pages/blogs/categories.json.ts
@@ -2,7 +2,7 @@ import type { APIContext, APIRoute } from 'astro';
 import { defineJsonEndpoint } from '@/api/response';
 import { fetchCategories } from '@/api/cms/fetch/category';
 
-export const prerender = true;
+export const prerender = false;
 
 export type GetRequest = Awaited<ReturnType<typeof get>>;
 export const GET: APIRoute = (context) => defineJsonEndpoint(get, context);
