071f36aceb6916d2a7a74609f864a7aca20265f1
junpei-8 <junpei.v8@gmail.com>
2024-05-17 13:34:58 +0900

    update: デプロイテストの修正

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -11,7 +11,7 @@ ssh-keyscan $VPS_HOST >> ~/.ssh/known_hosts
 echo "Host: $VPS_HOST"
 echo "Port: $VPS_PORT"
 echo "User: $VPS_USER"
-echo "cat ~/.ssh/vps"
+cat ~/.ssh/vps
 
 # # SSH 接続設定の重複削除
 # sort -u ~/.ssh/known_hosts -o ~/.ssh/known_hosts
